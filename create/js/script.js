a7a970f20fc1f0d991001e1cec248c74342f267b
VAC <37587399+vacnex@users.noreply.github.com>
2020-07-20 23:24:55 +0700

    Renew
    - Redesign ui because previous ui not responsive
    - Remove drag drop feature
    - The form has added sample inputs (just only input component)
    - modal for title with color autocomplete
    - modal for input component with update delete

--- create/js/script.js
+++ create/js/script.js
@@ -1,20 +1,198 @@
 $(document).ready(function(){
+/* #region  Init */
     $('#tabs-swipe').tabs({ swipeable: false });
     $('select').formSelect();
+    $('.modal').modal({onOpenStart: function(){
+        $('#iplabelmodal').val(val);
+        $( "#chkrequire" ).prop( "checked", chkstatus );
+    }});
+    $('.select-dropdown').css('cursor', 'pointer');
+    $('input.autocomplete').autocomplete({
+        data: {
+            "red": null,
+            "pink": null,
+            "purple": null,
+            "deep-purple": null,
+            "indigo": null,
+            "blue": null,
+            "light-blue": null,
+            "cyan": null,
+            "teal": null,
+            "green": null,
+            "light-green": null,
+            "lime": null,
+            "yellow": null,
+            "amber": null,
+            "orange": null,
+            "deep-orange": null,
+            "brown": null,
+            "grey": null,
+            "blue-grey": null,
+        },
+        onAutocomplete: function(val) {
+            if (val == 'red') {
+                $('#red').show();
+                $("#pink,#purple,#deep-purple,#indigo,#blue,#light-blue,#cyan,#teal,#green,#light-green,#lime,#yellow,#amber,#orange,#deep-orange,#brown,#grey,#blue-grey").hide();
+            }
+            if (val == 'pink') {
+                $('#pink').show();
+                $("#red,#purple,#deep-purple,#indigo,#blue,#light-blue,#cyan,#teal,#green,#light-green,#lime,#yellow,#amber,#orange,#deep-orange,#brown,#grey,#blue-grey").hide();
+            }if (val == 'purple') {
+                $('#purple').show();
+                $("#pink,#red,#deep-purple,#indigo,#blue,#light-blue,#cyan,#teal,#green,#light-green,#lime,#yellow,#amber,#orange,#deep-orange,#brown,#grey,#blue-grey").hide();
+            }if (val == 'deep-purple') {
+                $('#deep-purple').show();
+                $("#pink,#purple,#red,#indigo,#blue,#light-blue,#cyan,#teal,#green,#light-green,#lime,#yellow,#amber,#orange,#deep-orange,#brown,#grey,#blue-grey").hide();
+            }if (val == 'indigo') {
+                $('#indigo').show();
+                $("#pink,#purple,#deep-purple,#red,#blue,#light-blue,#cyan,#teal,#green,#light-green,#lime,#yellow,#amber,#orange,#deep-orange,#brown,#grey,#blue-grey").hide();
+            }if (val == 'blue') {
+                $('#blue').show();
+                $("#pink,#purple,#deep-purple,#indigo,#red,#light-blue,#cyan,#teal,#green,#light-green,#lime,#yellow,#amber,#orange,#deep-orange,#brown,#grey,#blue-grey").hide();
+            }if (val == 'light-blue') {
+                $('#light-blue').show();
+                $("#pink,#purple,#deep-purple,#indigo,#blue,#red,#cyan,#teal,#green,#light-green,#lime,#yellow,#amber,#orange,#deep-orange,#brown,#grey,#blue-grey").hide();
+            }if (val == 'teal') {
+                $('#teal').show();
+                $("#pink,#purple,#deep-purple,#indigo,#blue,#light-blue,#cyan,#red,#green,#light-green,#lime,#yellow,#amber,#orange,#deep-orange,#brown,#grey,#blue-grey").hide();
+            }if (val == 'cyan') {
+                $('#cyan').show();
+                $("#pink,#purple,#deep-purple,#indigo,#blue,#light-blue,#red,#teal,#green,#light-green,#lime,#yellow,#amber,#orange,#deep-orange,#brown,#grey,#blue-grey").hide();
+            }if (val == 'green') {
+                $('#green').show();
+                $("#pink,#purple,#deep-purple,#indigo,#blue,#light-blue,#cyan,#teal,#red,#light-green,#lime,#yellow,#amber,#orange,#deep-orange,#brown,#grey,#blue-grey").hide();
+            }if (val == 'light-green') {
+                $('#light-green').show();
+                $("#pink,#purple,#deep-purple,#indigo,#blue,#light-blue,#cyan,#teal,#green,#red,#lime,#yellow,#amber,#orange,#deep-orange,#brown,#grey,#blue-grey").hide();
+            }if (val == 'lime') {
+                $('#lime').show();
+                $("#pink,#purple,#deep-purple,#indigo,#blue,#light-blue,#cyan,#teal,#green,#light-green,#red,#yellow,#amber,#orange,#deep-orange,#brown,#grey,#blue-grey").hide();
+            }if (val == 'amber') {
+                $('#amber').show();
+                $("#pink,#purple,#deep-purple,#indigo,#blue,#light-blue,#cyan,#teal,#green,#light-green,#lime,#red,#red,#orange,#deep-orange,#brown,#grey,#blue-grey").hide();
+            }if (val == 'orange') {
+                $('#orange').show();
+                $("#pink,#purple,#deep-purple,#indigo,#blue,#light-blue,#cyan,#teal,#green,#light-green,#lime,#yellow,#amber,#red,#deep-orange,#brown,#grey,#blue-grey").hide();
+            }if (val == 'deep-orange') {
+                $('#deep-orange').show();
+                $("#pink,#purple,#deep-purple,#indigo,#blue,#light-blue,#cyan,#teal,#green,#light-green,#lime,#yellow,#amber,#orange,#red,#brown,#grey,#blue-grey").hide();
+            }
+            if (val == 'brown') {
+                $('#brown').show();
+                $("#pink,#purple,#deep-purple,#indigo,#blue,#light-blue,#cyan,#teal,#green,#light-green,#lime,#yellow,#amber,#orange,#deep-orange,#red,#grey,#blue-grey").hide();
+            }
+            if (val == 'grey') {
+                $('#grey').show();
+                $("#pink,#purple,#deep-purple,#indigo,#blue,#light-blue,#cyan,#teal,#green,#light-green,#lime,#yellow,#amber,#orange,#deep-orange,#brown,#red,#blue-grey").hide();
+            }
+            if (val == 'blue-grey') {
+                $('#blue-grey').show();
+                $("#pink,#purple,#deep-purple,#indigo,#blue,#light-blue,#cyan,#teal,#green,#light-green,#lime,#yellow,#amber,#orange,#deep-orange,#brown,#grey,#red").hide();
+            }
+        },
+    });
+/* #endregion */
+$('#autocomplete-input').on('keyup',function (e) { 
+    if ($('#autocomplete-input').val()== '') {
+        $("#pink,#purple,#deep-purple,#indigo,#blue,#light-blue,#cyan,#teal,#green,#light-green,#lime,#yellow,#amber,#orange,#deep-orange,#brown,#grey,#red,#blue-grey").show();
+    }
+});
+/* #region  Tab component effect */
     $('.itm').click(function (e) { 
-        $('.itm').removeClass('btn waves-effect waves-light').addClass('sa');
-        $(this).addClass('btn waves-effect waves-light').removeClass('sa');
+        $('.itm').removeClass('btn blue btn-flat').addClass('btn-flat');
+        $(this).addClass('btn blue').removeClass('btn-flat');
     });
-    $( ".input-field" ).draggable({ 
-        revert: true, 
-        helper: "clone" 
+    $('#ipfieldtab').on('click', function () {
+        $('#chkrdo').fadeOut(800).hide();
+        $('#slct').fadeOut(800).hide();
+        $('#inputfiled').fadeIn(800);
     });
-    $( ".cbrd" ).draggable({ 
-        revert: true, 
-        helper: "clone" 
+    $('#chkrdotab').on('click', function () {
+        $('#inputfiled').fadeOut(800).hide();
+        $('#slct').fadeOut(800).hide();
+        $('#chkrdo').fadeIn(800);
     });
-    $( ".ttl" ).draggable({ 
-        revert: true, 
-        helper: "clone" 
+    $('#selecttab').on('click', function () {
+        $('#chkrdo').fadeOut(800).hide();
+        $('#inputfiled').fadeOut(800).hide();
+        $('#slct').fadeIn(800);
     });
+/* #endregion */
+
+/* #region input text Modal */
+    var currentid = '#'
+    var iptextchild = '#'
+    var labeltextchild = '#'
+    var type = ''
+    var val = ''
+    var chkstatus
+    $('.previewform').on('click','.current', function () {
+        currentid = '#'
+        iptextchild = '#'
+        labeltextchild = '#'
+        type = $(this).children("input").prop('type')
+        currentid += $(this).attr('id');
+        iptextchild += $(this).children("input").attr('id')
+        val = $(this).children("label").text()
+        chkstatus = $(this).children("input").prop('required')
+        labeltextchild += $(this).children("label").attr('id')
+    });
+    $('#textmodalupdate').on('click', function () {
+        console.log(currentid)
+        var label = $('#iplabelmodal').val();
+        var required = $('#chkrequire').prop('checked')
+
+        if (label == '') {
+            $(labeltextchild).text('Default label');
+        } else{
+            $(labeltextchild).text(label);
+        }
+        if (required == true) {
+            $(iptextchild).prop('required',true);
+        }
+        else{
+            $(iptextchild).removeAttr('required');
+        }
+    });
+    $('#textmodaldelete').on('click', function () {
+        $(currentid).remove();
+    });
+
+/* #endregion */
+
+/* #region  inputfiled  */
+    var number
+    $('#ipfieldTemp').on('click', function () {
+        number = $('.previewform li').length + 1
+        $('.previewform').append('<li id="ip'+number+'" class="current input-field modal-trigger" href="#textmodal"><input id="defalttext'+number+'" type="text" class="validate"><label id="defalttextlabel'+number+'" for="defalttext'+number+'">Default label</label></li>');
+    });
+    $('#ipemailTemp').on('click', function () {
+        number = $('.previewform li').length + 1
+        $('.previewform').append('<li id="ip'+number+'" class="current input-field modal-trigger" href="#textmodal"><input id="defaltemail'+number+'" type="email" class="validate"><label id="defaltemaillabel'+number+'" for="defaltemail'+number+'">Default email label</label></li>');
+    });
+    $('#txtareaTemp').on('click', function () {
+        number = $('.previewform li').length + 1
+        $('.previewform').append('<div id="ip'+number+'" class="current input-field modal-trigger" href="#textmodal"><textarea id="txtarea'+number+'" type="email" class="validate materialize-textarea"></textarea><label id="txtareadefaultlabel'+number+'" for="txtarea'+number+'">Default text area label</label></div>');
+    });
+/* #endregion */
+/* #region  checkbox - radio */
+    $('#chkstyle1').on('click', function () {
+        
+    });
+    $('#chkstyle2').on('click', function () {
+        
+    });
+    $('#rdostyle1').on('click', function () {
+        
+    });
+    $('#rdostyle2').on('click', function () {
+        
+    });
+ /* #endregion */
+
+// $('#colormodal').on('keyup',function () { 
+//     var value = $(this).val().toLowerCase();
+//     console.log($('.dynamic-color').find('div[id^='+value+']'))
+// });
+
 });
